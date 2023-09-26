7e9706cf2c8910636232c5e8f739580391e7b5b4
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-09-26 15:22:30 -0700

    Defining project data structure

---
+++ projects.js
@@ -0,0 +1,19 @@
+// Define an array of projects as JSON objects
+const projectsData = [
+    {
+        title: "Project Title 1",
+        date: "Date: January 2023",
+        description: "This is a brief description of project 1. You can provide details about the project here.",
+        skills: ["HTML", "CSS", "JavaScript"],
+        link: "project1-link",
+        images: ["project1-image1.jpg", "project1-image2.jpg", "project1-image3.jpg"],
+    },
+    {
+        title: "Project Title 2",
+        date: "Date: March 2023",
+        description: "This is a brief description of project 2. You can provide details about the project here.",
+        link: "project2-link",
+        images: ["project2-image1.jpg", "project2-image2.jpg", "project2-image3.jpg"],
+    },
+    // Add more projects as needed
+];
