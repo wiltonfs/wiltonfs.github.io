2d9473d3624712117d3db8a9429e146d2f0d800d
wiltonfs <91587474+wiltonfs@users.noreply.github.com>
2023-09-26 15:39:25 -0700

    Game Dev data defined

--- projects.js
+++ projects.js
@@ -1,19 +1,24 @@
 // Define an array of projects as JSON objects
 const projectsData = [
     {
-        title: "Project Title 1",
-        date: "Date: January 2023",
-        description: "This is a brief description of project 1. You can provide details about the project here.",
-        skills: ["HTML", "CSS", "JavaScript"],
-        link: "project1-link",
-        images: ["project1-image1.jpg", "project1-image2.jpg", "project1-image3.jpg"],
+        title: "Twenty Flappy Eight",
+        subtitle: "Personal project",
+        date: "Jan 2023 - Feb 2023",
+        images: ["TwentyFlappy_0", "TwentyFlappy_1", "TwentyFlappy_2"],
+        description: "Combines the frantic arcade gameplay of Flappy Bird with the strategic number matching of 2048.",
+        skills: ["Unity", "C#"],
+        links_text: ["See project files on GitHub", "Try it out on Itch.io!"],
+        links: ["http://github.com/wiltonfs/TwentyFlappyEight", "http://wiltonfs.itch.io/twentyflappyeight"],
+        
     },
     {
-        title: "Project Title 2",
-        date: "Date: March 2023",
-        description: "This is a brief description of project 2. You can provide details about the project here.",
-        link: "project2-link",
-        images: ["project2-image1.jpg", "project2-image2.jpg", "project2-image3.jpg"],
+        title: "Economic Schools of Thought",
+        subtitle: "Final project for AP Macroeconomics",
+        date: "Jan 2019 - Feb 2019",
+        images: ["Economics_0", "Economics_1", "Economics_2"],
+        description: "Fast paced arcade game built in Scratch that teaches the player about different schools of economic thought.",
+        skills: ["Scratch", "Character balancing", "Educational game design"],
+        links_text: ["Try it out on Scratch!"],
+        links: ["https://scratch.mit.edu/projects/287915728/"], 
     },
-    // Add more projects as needed
 ];
